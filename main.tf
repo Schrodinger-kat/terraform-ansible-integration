36220f9d657d00b488a7c8f3d0cf0a6533b260ae
Jishnn <42387730+Schrodinger-kat@users.noreply.github.com>
2021-10-18 18:40:55 +0000

    fixes: indentation fixes

--- main.tf
+++ main.tf
@@ -1,58 +1,65 @@
 provider "google" {
- project     = var.project_id
- region = "us-east1"
+project = var.project_id
+region = var.hoenn
 }
-
+ 
 #Networks
 resource "google_compute_network" "pokenav" {
-    name = var.vpc
-    auto_create_subnetworks = false
+   name = var.pokenav
+   auto_create_subnetworks = false
 }
-
+ 
 #Subnet
 resource "google_compute_subnetwork" "hoenn" {
-    name = var.hoenn_sub
-    region = var.hoenn
-    ip_cidr_range = var.hoenn_id_cidr
-    network = google_compute_network.pokenav.self_link
-    private_ip_google_access = true
+   name = var.hoenn_sub
+   region = var.hoenn
+   ip_cidr_range = var.hoenn_id_cidr
+   network = google_compute_network.pokenav.self_link
+   private_ip_google_access = true
 }
-
+ 
 #Firewall
 resource "google_compute_firewall" "charizard" {
-    name = "charizard"
-    network       = google_compute_network.pokenav.self_link
-  direction     = "INGRESS"
-  source_ranges = ["10.0.0.0/8"]
-  
+   name = "charizard"
+   network       = google_compute_network.pokenav.self_link
+ direction     = "INGRESS"
+ source_tags =["oakspc-ssh"]
+ source_ranges = ["0.0.0.0/0"]
   allow {
-    protocol = "all"
-    ports    = []
-    }
+   protocol ="icmp"
+ }
+ allow {
+   protocol = "tcp"
+   ports    = ["80","8080","22"]
+   }
 }
-
+ 
 #Compute Engine
 resource "google_compute_instance" "oakpc" {
-    name = "Oak's PC"
-    machine_type = var.machine_type
-    zone = var.hoenn
-   
-    boot_disk {
-    initialize_params {
-      image = var.rom
-    }
+   name = "oakspc"
+   machine_type = var.machine_type
+   zone = var.oldale
+ 
+   boot_disk {
+   initialize_params {
+     image = var.rom
+   }
+ }
+ network_interface {
+   network    = google_compute_network.pokenav.self_link
+   subnetwork = google_compute_subnetwork.hoenn.self_link
+   access_config {
+    
+   }
+ }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${google_compute_instance.oakpc.network_interface.0.access_config.0.assigned_nat_ip},' --private-key ${var.private_key_path} tomcat.yml"
   }
-  network_interface {
-    network    = google_compute_network.pokenav.self_link
-    subnetwork = google_compute_subnetwork.hoenn.self_link
-    access_config {
-      
-    }
-  }
+ 
 }
-
+ 
 #Bucket
 resource "google_storage_bucket" "billpc" {
-    name = "Item Storage System"
-    location = var.hoenn  
+   name = "itemstoragesystem"
+   location = var.hoenn 
 }
